072ef3e4e1a37ad8792b92a471d6346d0720782f
patricia.cajaljal <patricia.cajaljal@synpulse.com>
2023-11-22 12:46:43 +0800

    [P8C-680] Updated schema service

--- src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/service/SchemaService.java
+++ src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/service/SchemaService.java
@@ -1,67 +1,14 @@
 package com.synpulse8.pulse8.core.accesscontrolsvc.service;
 
-import com.authzed.api.v1.*;
-import com.authzed.grpcutil.BearerToken;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import jakarta.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import com.authzed.api.v1.SchemaServiceOuterClass;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
-public class SchemaService {
-    private static Logger LOGGER = LoggerFactory.getLogger(SchemaService.class);
-    private ManagedChannel channel;
-    private SchemaServiceGrpc.SchemaServiceBlockingStub schemaService;
-    private PermissionsServiceGrpc.PermissionsServiceBlockingStub permissionsService;
+public interface SchemaService {
+    CompletableFuture<SchemaServiceOuterClass.ReadSchemaResponse> readSchema(SchemaServiceOuterClass.ReadSchemaRequest request);
 
-    @Value("${SPICEDB_HOST}")
-    private String spicedbHost;
-
-    @Value("${SPICEDB_PORT}")
-    private int spicedbPort;
-
-    @Value("${SPICEDB_PRESHARED_KEY}")
-    private String spiceDbPresharedKey;
-
-    @PostConstruct
-    public void initialize() {
-        LOGGER.info("Starting schema service");
-        // Check if the channel is already initialized
-        if (channel == null || channel.isTerminated()) {
-            channel = ManagedChannelBuilder
-                    .forAddress(spicedbHost, spicedbPort)
-                    // If SpiceDB requires TLS:
-//                     .useTransportSecurity()
-                    .usePlaintext()
-                    .build();
-        }
-        BearerToken bearerToken = new BearerToken(spiceDbPresharedKey);
-        // Check if the permissionsService is already initialized
-        if (schemaService == null) {
-            schemaService = SchemaServiceGrpc.newBlockingStub(channel)
-                    .withCallCredentials(bearerToken);
-        }
-        // Check if the permissionsService is already initialized
-        if (permissionsService == null) {
-            // Initialize the permissionsService only if not already initialized
-            permissionsService = PermissionsServiceGrpc.newBlockingStub(channel)
-                    .withCallCredentials(bearerToken);
-        }
-        LOGGER.info("Schema service started");
-    }
-
-    public String getSchema() {
-        SchemaServiceOuterClass.ReadSchemaRequest request = SchemaServiceOuterClass.ReadSchemaRequest
-                .newBuilder()
-                .build();
-
-        SchemaServiceOuterClass.ReadSchemaResponse response;
-        response = schemaService.readSchema(request);
-        String schema = response.getSchemaText();
-        return schema;
-    }
+    CompletableFuture<SchemaServiceOuterClass.WriteSchemaResponse> writeSchema(SchemaServiceOuterClass.WriteSchemaRequest request);
 
 }
---
+++ src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/service/SchemaServiceImpl.java
@@ -0,0 +1,43 @@
+package com.synpulse8.pulse8.core.accesscontrolsvc.service;
+
+import com.authzed.api.v1.SchemaServiceGrpc;
+import com.authzed.api.v1.SchemaServiceOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+@Service
+public class SchemaServiceImpl implements SchemaService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SchemaServiceImpl.class);
+    private final SchemaServiceGrpc.SchemaServiceBlockingStub schemaService;
+
+    @Autowired
+    public SchemaServiceImpl(SchemaServiceGrpc.SchemaServiceBlockingStub schemaService) {
+        this.schemaService = schemaService;
+    }
+
+    @Override
+    public CompletableFuture<SchemaServiceOuterClass.ReadSchemaResponse> readSchema(SchemaServiceOuterClass.ReadSchemaRequest request) {
+        return runAsyncTask(() -> schemaService.readSchema(request));
+    }
+
+    @Override
+    public CompletableFuture<SchemaServiceOuterClass.WriteSchemaResponse> writeSchema(SchemaServiceOuterClass.WriteSchemaRequest request) {
+        return runAsyncTask(() -> schemaService.writeSchema(request));
+    }
+
+    private <T> CompletableFuture<T> runAsyncTask(Supplier<T> task) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return task.get();
+            } catch (Exception e) {
+                LOGGER.error("Error while processing request", e);
+                throw new RuntimeException("Error while processing request", e);
+            }
+        });
+    }
+}
