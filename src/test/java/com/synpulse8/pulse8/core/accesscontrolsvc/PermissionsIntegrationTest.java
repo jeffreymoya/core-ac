f64f0b70015f116c99aa1ba739eef6bf4bbc1af2
Jeffrey Moya <jeffreymoya@gmail.com>
2023-11-29 16:16:59 +0800

    P8C-723 added testcontainer for spicedb

--- src/test/java/com/synpulse8/pulse8/core/accesscontrolsvc/PermissionsIntegrationTest.java
+++ src/test/java/com/synpulse8/pulse8/core/accesscontrolsvc/PermissionsIntegrationTest.java
@@ -1,15 +1,21 @@
 package com.synpulse8.pulse8.core.accesscontrolsvc;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -21,10 +27,32 @@ import org.springframework.test.context.ActiveProfiles;
 @CucumberContextConfiguration
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EnableAutoConfiguration(exclude= {
-        SecurityAutoConfiguration.class,
-        SecurityFilterAutoConfiguration.class,
-        ManagementWebSecurityAutoConfiguration.class})
 public class PermissionsIntegrationTest {
+
+    @ClassRule
+    public static GenericContainer<?> spicedb = new GenericContainer<>(
+            DockerImageName.parse("authzed/spicedb:latest"))
+            .withExposedPorts(50051)
+            .withCommand("serve --grpc-preshared-key integration_test-key")
+            .waitingFor(Wait.forHealthcheck())
+            .waitingFor(Wait.forListeningPort())
+            .withStartupTimeout(Duration.ofMinutes(2))
+            .waitingFor(Wait.forLogMessage(".*grpc server started serving.*\\n", 1));
+
+    @DynamicPropertySource
+    static void spicedbProperties(DynamicPropertyRegistry registry) {
+        registry.add("SPICEDB_HOST", spicedb::getHost);
+        registry.add("SPICEDB_PORT", () -> spicedb.getFirstMappedPort());
+        registry.add("SPICEDB_PRESHARED_KEY", () -> "integration_test-key");
+    }
+    @BeforeAll
+    public static void setUp() {
+        spicedb.start();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        spicedb.stop();
+    }
 
 }
--- src/test/resources/permissions.feature
+++ src/test/resources/permissions.feature
@@ -1,7 +1,12 @@
 Feature: Permissions API
 
+  Scenario: Principal header is not set
+    Given the API is available
+    When a user checks permissions with principal ""
+    Then the response code should be 403
+
   Scenario: Check Permissions
     Given the API is available
-    When a user checks permissions
+    When a user checks permissions with principal "1234"
     Then the response code should be 500
     And the response should contain "FAILED_PRECONDITION: object definition `blog/post` not found"
