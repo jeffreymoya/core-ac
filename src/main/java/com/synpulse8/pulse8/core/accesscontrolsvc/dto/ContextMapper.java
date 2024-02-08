aeb0e7533b71f21f51a0d57656cecca5babee3ad
patricia.cajaljal <patricia.cajaljal@synpulse.com>
2024-02-08 12:41:09 +0800

    [P8C-917] Added value types on context mapper

--- src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/dto/ContextMapper.java
+++ src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/dto/ContextMapper.java
@@ -1,7 +1,11 @@
 package com.synpulse8.pulse8.core.accesscontrolsvc.dto;
 
+import com.google.protobuf.ListValue;
+import com.google.protobuf.Struct;
 import com.google.protobuf.Value;
+import com.synpulse8.pulse8.core.accesscontrolsvc.exception.P8CException;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -15,13 +19,26 @@ public class ContextMapper {
                 ));
     }
 
+    private static ListValue.Builder convertList(List list) {
+        ListValue.Builder listValueBuilder = ListValue.newBuilder();
+        list.forEach(item -> listValueBuilder.addValues(toValue(item)));
+        return listValueBuilder;
+    }
+
     private static Value toValue(Object value) {
-        try {
-            return Value.newBuilder()
-                    .setStringValue(value.toString())
-                    .build();
-        } catch (Exception e) {
-            throw new RuntimeException("Error converting object to Value", e);
+        Value.Builder builder = Value.newBuilder();
+        if (value instanceof String) return builder.setStringValue(value.toString()).build();
+        else if (value instanceof Integer) return builder.setNumberValue((Integer) value).build();
+        else if (value instanceof Double) return builder.setNumberValue((Double) value).build();
+        else if (value instanceof Boolean) return builder.setBoolValue((Boolean) value).build();
+        else if (value instanceof List) return builder.setListValue(convertList((List) value).build()).build();
+        else if (value instanceof Map) {
+            Struct.Builder structBuilder = Struct.newBuilder().putAllFields(ContextMapper.convertMap((Map) value));
+            return builder.setStructValue(structBuilder.build()).build();
+        }
+        else if (value == null) return builder.setNullValueValue(0).build();
+        else {
+            throw new P8CException("Unsupported type for value: " + value);
         }
     }
 }
