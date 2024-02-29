990140a371fe2c6c90ba7aae1f9da89d1dcd7845
jovielene-lanorio_SYNPULSE <jovielene.lanorio@synpulse.com>
2024-02-29 15:45:02 +0800

    P8C-822 set profile configuration for the kafka controller

--- src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/controller/kafka/KafkaEventController.java
+++ src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/controller/kafka/KafkaEventController.java
@@ -13,12 +13,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Profile("development")
 @OpenAPIDefinition(
         info = @Info(title = "Kafka Event Controller API", version = "v1"),
         security = @SecurityRequirement(name = "X-Authenticated-User"))
