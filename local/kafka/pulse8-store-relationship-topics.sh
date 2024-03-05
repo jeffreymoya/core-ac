5786ff04e88ec79253ab781f54fdc13a71e53f22
patricia.cajaljal <patricia.cajaljal@synpulse.com>
2024-03-05 18:20:10 +0800

    [P8C-617] Added topic and log file for role audit logs

--- local/kafka/pulse8-store-relationship-topics.sh
+++ local/kafka/pulse8-store-relationship-topics.sh
@@ -5,6 +5,7 @@ create_topic() {
         create-relationship,
         delete-relationship
         logs-attributes
+        logs-roles
      )
         echo -e "zookeeper:2182 partition 1 replication-factor 1"
         for topicName in ${topicList[*]}; do
