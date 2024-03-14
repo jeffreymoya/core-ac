171fd44fa9f1427a7645167a8cdcfee87d1f2e1d
jovielene-lanorio_SYNPULSE <jovielene.lanorio@synpulse.com>
2024-03-14 14:47:07 +0800

    P8C-922 changes for updating multiple relation

--- src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/kafka/consumer/UpdateRelationshipConsumer.java
+++ src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/kafka/consumer/UpdateRelationshipConsumer.java
@@ -2,6 +2,7 @@ package com.synpulse8.pulse8.core.accesscontrolsvc.kafka.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.synpulse8.pulse8.core.RelationshipCreation;
 import com.synpulse8.pulse8.core.RelationshipUpdate;
 import com.synpulse8.pulse8.core.accesscontrolsvc.dto.DeleteRelationshipRequestDto;
@@ -35,17 +36,36 @@ public class UpdateRelationshipConsumer {
     @KafkaListener(topics = "${p8c.kafka.topics.update_relationship}" , groupId = "${spring.kafka.consumer.group-id}")
     public void updateRelationship(ConsumerRecord<String, RelationshipUpdate> record) {
 
-        String updates = "{\"updates\":[" + record.value() + "]}";
+        if(record.value().getRelation() != null && !record.value().getRelation().isEmpty()){
+            ArrayList<String> relationList = new ArrayList<>(Arrays.asList(record.value().getRelation().split(",")));
+            StringBuilder recordListBuilder = new StringBuilder();
 
-        try {
+            relationList.stream().forEach(x -> {
+                RelationshipUpdate relationshipUpdate = new RelationshipUpdate();
+                relationshipUpdate.setObjectType(record.value().getObjectType());
+                relationshipUpdate.setObjectId(record.value().getObjectId());
+                relationshipUpdate.setRelation(x);
+                relationshipUpdate.setSubjRefObjId(record.value().getSubjRefObjId());
+                relationshipUpdate.setSubjRefObjType(record.value().getSubjRefObjType());
+                relationshipUpdate.setSubjRelation(record.value().getSubjRelation());
+                relationshipUpdate.setOperation("OPERATION_TOUCH");
+                recordListBuilder.append(relationshipUpdate).append(",");
+            });
 
-            WriteRelationshipRequestDto writeRelationshipRequestDto = new ObjectMapper().readValue(updates, WriteRelationshipRequestDto.class);
-            log.info("Consumed details used for updating relationship :\n" + writeRelationshipRequestDto.toWriteRelationshipRequest());
-            permissionsService.writeRelationships(writeRelationshipRequestDto.toWriteRelationshipRequest());
+            String updates = "{\"updates\":[" + recordListBuilder.substring(0, recordListBuilder.length() - 1) + "]}";
 
-        } catch (JsonProcessingException e) {
-            log.debug(String.format("Error updating relationship caused by: " + e.getMessage()));
-            throw new P8CException("Error updating relationship.");
+            try {
+
+                WriteRelationshipRequestDto writeRelationshipRequestDto = new ObjectMapper().readValue(updates, WriteRelationshipRequestDto.class);
+                log.info("Consumed details used for updating relationship :\n" + writeRelationshipRequestDto.toWriteRelationshipRequest());
+                permissionsService.writeRelationships(writeRelationshipRequestDto.toWriteRelationshipRequest());
+
+            } catch (JsonProcessingException e) {
+                log.debug(String.format("Error updating relationship caused by: " + e.getMessage()));
+                throw new P8CException("Error updating relationship.");
+            }
+
+            latch.countDown();
         }
     }
 
