c7e478b7f54f63d86758dbf608ffb5d9b9f9d905
patricia.cajaljal <patricia.cajaljal@synpulse.com>
2024-03-14 17:04:48 +0800

    [P8C-1023] Added config profile for consumer toggle

--- src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/synpulse8/pulse8/core/accesscontrolsvc/kafka/consumer/KafkaConsumerConfig.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 @Configuration
+@Profile("!test")
 public class KafkaConsumerConfig {
 
     @Autowired
